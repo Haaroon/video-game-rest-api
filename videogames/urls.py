b2e33da9d5d1e1d756ea11d78514446ca99acd8b
Haaroon Yousaf <haaroon.yousaf@gmail.com>
2016-03-17 10:15:30 +0000

    completed part 3

--- videogames/urls.py
+++ videogames/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from videogames import views
 
 urlpatterns = [
-    url(r'^all/$', views.videogame_list),
-    url(r'^game_detail/(?P<pk>[0-9]+)/$', views.videogame_detail),
+    url(r'^all/$', views.VideoGameList.as_view()),
+    url(r'^game_detail/(?P<pk>[0-9]+)/$', views.VideoGameDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- videogames/views.py
+++ videogames/views.py
@@ -1,63 +1,14 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
+from videogames.models import *
+from videogames.serializers import *
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework import status
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from videogames.models import VideoGame
-from videogames.serializers import VideoGameSerializer
-from rest_framework.request import Request
-from rest_framework.test import APIRequestFactory
+from rest_framework import generics
 
-# TODO REMOVE THIS EXEMPTIOn
-@api_view(['GET'] ) #, 'POST'])
-def videogame_list(request, format=None):
-    serializer_context = {
-        'request': request,
-    }
-    #   List all games, or create a new game.
-    if request.method == 'GET':
-        videogames = VideoGame.objects.all()
-        print("Video games get ", videogames)
-        serializer = VideoGameSerializer(videogames, many=True, context=serializer_context)
-        return Response(serializer.data)
+class VideoGameList(generics.ListCreateAPIView):
+    queryset = VideoGame.objects.all()
+    serializer_class = VideoGameSerializer
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = VideoGameSerializer(data=data, context=serializer_context)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET']) # 'PUT'])
-def videogame_detail(request, pk, format=None):
-    serializer_context = {
-        'request': request,
-    }
-
-    """
-    Retrieve, update or delete a videogame.
-    """
-    try:
-        videogame = VideoGame.objects.get(pk=pk)
-    except VideoGame.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = VideoGameSerializer(videogame, context=serializer_context)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = VideoGameSerializer(videogame, data=data, context=serializer_context)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # elif request.method == 'DELETE':
-    #     videogame.delete()
-    #     return HttpResponse(status=204)
+class VideoGameDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = VideoGame.objects.all()
+    serializer_class = VideoGameSerializer
