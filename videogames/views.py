a2052336b2234c2b71d826ac05cbc77852d6c0f8
Haaroon Yousaf <haaroon.yousaf@gmail.com>
2016-03-28 14:15:18 +0100

    fixed search, added review search

--- videogames/views.py
+++ videogames/views.py
@@ -38,6 +38,28 @@ class ReviewViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(username=self.request.user)
 
+    def get_queryset(self):
+        print("In get query set, query params is : ", self.request.query_params)
+        queryset = Review.objects.all()
+        searchHeading = self.request.query_params.get('heading', None)
+        searchBody = self.request.query_params.get('body', None)
+        searchRating = self.request.query_params.get('rating', None)
+        searchGame = self.request.query_params.get('game', None)
+        searchUsername = self.request.query_params.get('username', None)
+
+        if searchHeading is not None:
+            queryset = queryset.filter(heading__icontains=searchHeading)
+        if searchBody is not None:
+            queryset = queryset.filter(body__icontains=searchBody)
+        if searchGame is not None:
+            queryset = queryset.filter(game__title__icontains=searchGame)
+        if searchRating is not None:
+            queryset = queryset.filter(rating=searchRating)
+        if searchUsername is not None:
+                queryset = queryset.filter(username__username__icontains=searchUsername)
+            
+        return queryset
+
 class VideoGameViewSet(viewsets.ModelViewSet):
     queryset = VideoGame.objects.all()
     serializer_class = VideoGameSerializer
@@ -49,18 +71,21 @@ class VideoGameViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         print("In get query set, query params is : ", self.request.query_params)
         queryset = VideoGame.objects.all()
+        searchTitle = self.request.query_params.get('title', None)
         searchGenre = self.request.query_params.get('genre', None)
         searchDeveloper = self.request.query_params.get('developer', None)
         searchPlatform = self.request.query_params.get('platform', None)
         searchRating = self.request.query_params.get('rating', None)
         searchOwner = self.request.query_params.get('owner', None)
 
+        if searchTitle is not None:
+            queryset = queryset.filter(title__icontains=searchTitle)
         if searchGenre is not None:
             queryset = queryset.filter(genre=searchGenre)
         if searchDeveloper is not None:
-            queryset = queryset.filter(developer=searchDeveloper)
+            queryset = queryset.filter(developer__developer__icontains=searchDeveloper)
         if searchPlatform is not None:
-            queryset = queryset.filter(platform=searchPlatform)
+            queryset = queryset.filter(platform__platform__icontains=searchPlatform)
         if searchRating is not None:
             queryset = queryset.filter(rating=searchRating)
         if searchOwner is not None:
